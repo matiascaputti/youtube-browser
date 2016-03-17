d5d26a0d0385465b01eada4326f5623f3eec4143
maticaputti <matias.caputti@gmail.com>
2016-03-17 18:36:08 -0300

    Add styles

--- src/index.jsx
+++ src/index.jsx
@@ -3,16 +3,23 @@ import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search-bar';
 import VideoList from './components/video-list';
+import VideoDetail from './components/video-detail';
 const API_KEY = 'AIzaSyAV7ITABgL1f0cnbxazocKid6qu-Wc1Kis';
 
 class App extends Component {
     constructor(props) {
         super(props);
 
-        this.state = { videos: [] };
+        this.state = {
+            videos: [],
+            selectedVideo: null
+        };
 
         YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
-            this.setState({ videos });
+            this.setState({
+                videos: videos,
+                selectedVideo: videos[0]
+            });
         });
     }
 
@@ -20,7 +27,10 @@ class App extends Component {
         return (
             <div>
                 <SearchBar />
-                <VideoList videos={this.state.videos} />
+                <VideoDetail video={this.state.selectedVideo} />
+                <VideoList
+                    onVideoSelect={selectedVideo => this.setState({selectedVideo}) }
+                    videos={this.state.videos} />
             </div>
         );
     }
